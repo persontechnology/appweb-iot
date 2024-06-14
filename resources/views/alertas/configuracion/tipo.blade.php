1470cc93da3095122a62e0fe3b4a28d5105fc17d
persontechnology <persontechnologys@gmail.com>
2024-06-14 11:50:46 -0500

    tipo set u-nss

--- resources/views/alertas/configuracion/tipo.blade.php
+++ resources/views/alertas/configuracion/tipo.blade.php
@@ -37,7 +37,7 @@
                                 </optgroup>
 
                                 <optgroup label="Enviroment Monitoring Sensor">
-                                    <option value="distance=====Distancia milimetros (30 to 4500)">Distancia milimetros (30 to 4500)</option>
+                                    <option value="distance=====Distancia en milimetros (30 to 4500)">Distancia en milimetros (30 to 4500)</option>
                                     <option value="position=====Posición del dispositivo (normal, tilt)">Posición del dispositivo (normal, tilt) </option>
                                     
                                 </optgroup>
