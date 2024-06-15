7f8e4b7770c04ffb7c3bb1edab497ed6b3662eb9
persontechnology <persontechnologys@gmail.com>
2024-06-15 07:35:56 -0500

    name users

--- database/migrations/2024_04_01_150253_add_atributos_to_user_table.php
+++ database/migrations/2024_04_01_150253_add_atributos_to_user_table.php
@@ -31,7 +31,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('user', function (Blueprint $table) {
-            $table->dropColumn(['password','apellidos','nombres','identificacion','email_verified_at','remember_token','tenant_id']);
+            $table->dropColumn(['password','apellidos','nombres','identificacion','email_verified_at','remember_token','tenant_id','name']);
         });
     }
 };
