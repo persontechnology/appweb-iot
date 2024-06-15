15b0f8f63949453d52c8b28345cda319635cdb09
persontechnology <persontechnologys@gmail.com>
2024-06-15 08:01:07 -0500

    endssss

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -17,7 +17,7 @@ class WelcomeController extends Controller
         // Artisan::call('key:generate');
         // Artisan::call('migrate:fresh --seed');
         $users = DB::table('migrations')->get();
-        $deleted = DB::table('migrations')->whereIn('id', [45,78,81])->delete();
+        $deleted = DB::table('migrations')->whereIn('id', [77,79])->delete();
 
         // Devolver los resultados como JSON
         return response()->json($users);
