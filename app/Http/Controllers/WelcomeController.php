2c961c050f6b8586fbc0927f99a5431a7244d2c9
persontechnology <persontechnologys@gmail.com>
2024-06-15 08:49:58 -0500

    111xsvanntlñsT

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class WelcomeController extends Controller
@@ -15,10 +16,7 @@ class WelcomeController extends Controller
         // Artisan::call('storage:link');
         // Artisan::call('key:generate');
         // Artisan::call('migrate:fresh --seed');
-        $user=User::where('email','persontechnologys@gmail.com')->first();
-        $user->password=Hash::make('12345678');
-        $user->tenant_id='52f14cd4-c6f1-4fbd-8f87-4025e1d49242';
-        $user->save();
+        DB::table('migrations')->truncate();
         return view('welcome');
         
     }
